2f0982faed787448994783e8b46c8cbce862f382
Probir-Ray <somironroy.need@gmail.com>
2021-08-21 01:16:15 +0600

    update

--- js/script.js
+++ js/script.js
@@ -1,70 +1,13 @@
 // Javascript started here.
 
-
-// Memory Cost function
-function getMemoryCost(price) {
-    const memoryCostText = document.getElementById('memory-price');
-    let memoryConst = memoryCostText.innerText;
-    memoryCostText.innerText = price;
+// Get Cost function
+function getCost(item, price) {
+    const itemCostText = document.getElementById(item + '-price');
+    let itemCostAmount = itemCostText.innerText;
+    itemCostText.innerText = price;
+    
     totalPriceCalc();
 }
-
-// Event Listener for 8GB Memory 
-document.getElementById('memory-8gb').addEventListener('click', function() {
-    getMemoryCost(0);
-});
-
-// Event Listener for 16GB Memory
-document.getElementById('memory-16gb').addEventListener('click', function() {
-    getMemoryCost(180);
-});
-
-
-
-// Storage Cost function
-function getStorageCost(price) {
-    const storageCostText = document.getElementById('storage-price');
-    let storageCost = storageCostText.innerText;
-    storageCostText.innerText = price;
-    totalPriceCalc();
-}
-
-// Event Listener for 256GB SSD
-document.getElementById('ssd-256').addEventListener('click', function() {
-    getStorageCost(0);
-})
-
-// Event Listener for 512GB SSD
-document.getElementById('ssd-512').addEventListener('click', function() {
-    getStorageCost(100);
-})
-
-// Event Listener for 1TB SSD
-document.getElementById('ssd-1tb').addEventListener('click', function() {
-    getStorageCost(180);
-})
-
-
-
-// Delivery Cost Calculate;
-function getDeliveryCost(costOfDelivery) {
-    const deliveryPriceText = document.getElementById('delivery-price');
-    let deliveryPrice = deliveryPriceText.innerText;
-    deliveryPriceText.innerText = costOfDelivery;
-    totalPriceCalc();
-}
-
-// Event Listener for free delivery
-document.getElementById('delivery-free').addEventListener('click', function() {
-    getDeliveryCost(0);
-})
-
-// Event Listener for paid delivery
-document.getElementById('delivery-paid').addEventListener('click', function() {
-    getDeliveryCost(20);
-})
-
-
 
 // Total Price Calculation
 function totalPriceCalc() {
@@ -72,13 +15,55 @@ function totalPriceCalc() {
     const totalPriceText = document.getElementById('total-price');
     let totalPrice = totalPriceText.innerText;
 
+    // Get Element value.
     const memoryCostText = document.getElementById('memory-price');
     const storageCostText = document.getElementById('storage-price');
     const deliveryPriceText = document.getElementById('delivery-price');
 
+    // Sum of all cost
     totalPrice = parseFloat(memoryCostText.innerText) + parseFloat(storageCostText.innerText) +
-    parseFloat(deliveryPriceText.innerText);
+    parseFloat(deliveryPriceText.innerText) + bestPrice;
 
-    totalPriceText.innerText = bestPrice + totalPrice;
+    totalPriceText.innerText = totalPrice;
 
 }
+
+
+// Event Listener for 8GB Memory 
+document.getElementById('memory-8gb').addEventListener('click', function() {
+    getCost('memory', 0);
+});
+
+// Event Listener for 16GB Memory
+document.getElementById('memory-16gb').addEventListener('click', function() {
+    getCost('memory', 180);
+});
+
+
+
+// Event Listener for 256GB SSD
+document.getElementById('ssd-256').addEventListener('click', function() {
+    getCost('storage', 0);
+})
+
+// Event Listener for 512GB SSD
+document.getElementById('ssd-512').addEventListener('click', function() {
+    getCost('storage', 100);
+})
+
+// Event Listener for 1TB SSD
+document.getElementById('ssd-1tb').addEventListener('click', function() {
+    getCost('storage', 180);
+})
+
+
+// Event Listener for free delivery
+document.getElementById('delivery-free').addEventListener('click', function() {
+    getCost('delivery', 0);
+})
+
+// Event Listener for paid delivery
+document.getElementById('delivery-paid').addEventListener('click', function() {
+    getCost('delivery', 20);
+})
+
